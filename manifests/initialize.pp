8ae016d8bbd5116cc21a07d0c3c78abcc4c26710
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-01-06 13:43:59 -0800

    Fix up gerrit initilization check and command
    The gerrit init command really should have the directory passed to it.
    Additionally instead of touching a file to say we've completed, the
    initialization ends up creating the All-Projects.git repo so lets look
    for the HEAD file in there.
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/initialize.pp
+++ manifests/initialize.pp
@@ -20,17 +20,18 @@
 # Copyright 2014 Andrew Grimberg
 #
 class gerrit::initialize {
-  $options        = $gerrit::options
-  $gerrit_home    = $gerrit::gerrit_home
-  $gerrit_version = $gerrit::gerrit_version
-  $gerrit_user    = $options['container']['user']
+  $options          = $gerrit::options
+  $gerrit_home      = $gerrit::gerrit_home
+  $gerrit_version   = $gerrit::gerrit_version
+  $gerrit_user      = $options['container']['user']
+  $gerrit_basepath  = $options['gerrit']['basePath']
 
   exec { 'gerrit_initialize':
     cwd     => $gerrit_home,
     path    => [ '/usr/bin', '/usr/sbin' ],
     command => "java -jar ${gerrit_home}/bin/gerrit-${gerrit_version}.war \
-init --batch && touch ${gerrit_home}/.gerrit_setup_complete.txt",
-    creates => "${gerrit_home}/.gerrit_setup_complete.txt",
+init -d ${gerrit_home} --batch",
+    creates => "${gerrit_basepath}/All-Projects.git/HEAD",
     user    => $gerrit_user,
   }
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -303,8 +303,8 @@ describe 'gerrit', :type => :class do
     context 'with defaults' do
       it { is_expected.to contain_exec('gerrit_initialize').with(
         'cwd'     => '/opt/gerrit',
-        'command' => 'java -jar /opt/gerrit/bin/gerrit-2.9.3.war init --batch && touch /opt/gerrit/.gerrit_setup_complete.txt',
-        'creates' => '/opt/gerrit/.gerrit_setup_complete.txt',
+        'command' => 'java -jar /opt/gerrit/bin/gerrit-2.9.3.war init -d /opt/gerrit --batch',
+        'creates' => '/srv/gerrit/All-Projects.git/HEAD',
         'path'    => [ '/usr/bin', '/usr/sbin' ],
         'user'    => 'gerrit',
       ) }
