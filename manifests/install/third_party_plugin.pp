e520583e402cde037dce0113b63c96fbf2845caf
Andrew Grimberg <agrimberg@linuxfoundation.org>
2015-12-02 09:22:37 -0800

    Add comment header to third_party_plugin define
    Add file header documentation to the install::third_party_plugin define.
    
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- manifests/install/third_party_plugin.pp
+++ manifests/install/third_party_plugin.pp
@@ -1,3 +1,42 @@
+# == Define: gerrit::install::third_party_plugin
+#
+# This define is used for fetching and installing 3rd party plugins into
+# Gerrit
+#
+# === Variables
+#
+# [*gerrit_home*]
+#   The home directory where Gerrit is being installed
+#
+# [*gerrit_user*]
+#   The user that gerrit is using
+#
+# [*plugin_source*]
+#   An appropriate fully qualified resource locator.
+#
+#   Valid resource types are file puppet and http(s)
+#
+# === Examples
+#
+# gerrit::install::third_party_plugin { 'delete-project':
+#   gerrit_home   => '/opt/gerrit',
+#   gerrit_user   => 'gerrit',
+#   plugin_source =>
+#   'https://gerrit-ci.gerritforge.com/view/Plugins-stable-2.11/job/plugin-delete-project-stable-2.11/lastSuccessfulBuild/artifact/buck-out/gen/plugins/delete-project/delete-project.jar',
+# }
+#
+# === Authors
+#
+# Andrew Grimberg <agrimberg@linuxfoundation.org>
+#
+# === Copyright
+#
+# Copyriight 2015 Andrew Grimberg
+#
+# === License
+#
+# @License Apache-2.0 <http://spdx.org/licenses/Apache-2.0.html>
+#
 define gerrit::install::third_party_plugin (
   $gerrit_home,
   $gerrit_user,
